f1153d682498dc93bf1693deabfccda45f19834f
teesloane <tylersloane@gmail.com>
2017-07-03 13:42:26 -0400

    Set up env variable check + send a dummy message on authentication!

--- index.js
+++ index.js
@@ -1,22 +1,66 @@
-// Import the slack client library for building bots
-var RtmClient = require("@slack/client").RtmClient;
-var CLIENT_EVENTS = require("@slack/client").CLIENT_EVENTS;
-var RTM_EVENTS = require("@slack/client").RTM_EVENTS;
+// NATIVE NODE LIBRARIES
+const readline = require("readline");
 
-// set up the library with necessary token and init the library
+// THIRD PARTY LIBRARIES
+const RtmClient = require("@slack/client").RtmClient;
+const CLIENT_EVENTS = require("@slack/client").CLIENT_EVENTS;
+const RTM_EVENTS = require("@slack/client").RTM_EVENTS;
+
+// CONSTANTS
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// VARIABLES
 var bot_token = process.env.SLACK_BOT_TOKEN || "";
-var rtm = new RtmClient(bot_token);
 
-// The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload if you want to cache it
-rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function(rtmStartData) {
-  console.log(
-    `Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}`
-  );
-});
+// check if slack bot token env var exists before booting the program
+if (process.env.SLACK_BOT_TOKEN) {
+  init_bot();
+} else {
+  rl.question("Please provide your SLACK_BOT_TOKEN for your team: ", answer => {
+    bot_token = answer;
+    rl.close();
+    init_bot();
+  });
+}
 
-// log an incoming message
-rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
-  console.log("Message:", message);
-});
+function init_bot() {
+  console.log("Initializing Penny...");
 
-rtm.start();
+  // ==================================
+  // BOT variables and Constants
+  // ==================================
+
+  var rtm = new RtmClient(bot_token);
+  let channel;
+
+  // =============================
+  // Real Time Event Hanlders
+  // =============================
+
+  // The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload if you want to cache it
+  rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function(rtmStartData) {
+    console.log(
+      `Initializing ${rtmStartData.self.name} for  ${rtmStartData.team.name}`
+    );
+
+    for (let c of rtmStartData.channels) {
+      if (c.is_member && c.name === "draw_it") {
+        channel = c.log;
+      }
+    }
+  });
+
+  // id an incoming message
+  rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
+    console.log("Message:", message);
+  });
+
+  rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function() {
+    // rtm.sendMessage("Hello I am Penny, I just connected from the server!", 'C63GFH05V');
+  });
+
+  rtm.start();
+}
